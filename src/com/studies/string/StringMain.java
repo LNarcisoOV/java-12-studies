3c9157335c244dd8a939b1be8eecb6a7ecd82cc9
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-11-02 15:23:31 +0000

    Add indent() example.

---
+++ src/com/studies/string/StringMain.java
@@ -0,0 +1,11 @@
+package com.studies.string;
+
+public class StringMain {
+
+	public static void main(String[] args) {
+		System.out.println("INDENT METHOD EXAMPLE:");
+		
+		String olaMundo = "Hello World!";
+		System.out.println(olaMundo.indent(10));
+	}
+}
