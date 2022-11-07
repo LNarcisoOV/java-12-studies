a0b400172679ececb7090ddc2e21b05f46c70f9d
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-11-07 14:49:14 +0000

    Add example for transform() method chained.

--- src/com/studies/string/StringMain.java
+++ src/com/studies/string/StringMain.java
@@ -1,11 +1,23 @@
 package com.studies.string;
 
 public class StringMain {
+	
+	private static final String HELLO_WORLD = "Hello World!";
 
 	public static void main(String[] args) {
 		System.out.println("INDENT METHOD EXAMPLE:");
 		
-		String olaMundo = "Hello World!";
-		System.out.println(olaMundo.indent(10));
+		String helloWorld = HELLO_WORLD;
+		System.out.println(helloWorld.indent(10));
+		
+		System.out.println("TRANSFORM METHOD EXAMPLE:");
+		helloWorld = HELLO_WORLD;
+		
+		helloWorld = helloWorld.transform(s -> s + "!!!!!!");
+		System.out.print(helloWorld);
+		
+		System.out.println();
+		helloWorld = helloWorld.transform(s -> s + "!!!!!!").transform(s -> s.concat(" Using transform() method twice."));
+		System.out.print(helloWorld);
 	}
 }
